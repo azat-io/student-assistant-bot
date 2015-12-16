dce8f87ae687250e3c5fb35ea9456b8f744bee8e
Azat S <to@azat.io>
2015-12-16 22:12:34 +0300

    Add start message

--- app.js
+++ app.js
@@ -5,5 +5,16 @@ var bot = new TelegramBot(token, {polling: true});
 bot.on('message', function (msg) {
   var chatId = msg.chat.id;
   console.log(msg);
-  bot.sendMessage(chatId, "Hello!", {caption: "I'm a bot!"});
+  if (msg.text == '/start') {
+    var opts = {
+      reply_markup: JSON.stringify({
+        keyboard: [
+          ['🇺🇸', '🇷🇺']
+        ],
+        resize_keyboard: true,
+        one_time_keyboard: true
+      })
+    };
+    bot.sendMessage(chatId, 'Hello!\n\nI\'m a Student Assistant Bot. And I want to help you in creating your own bot that will help you pass the exam. Before we begin, please choose your language.', opts);
+  }
 });
